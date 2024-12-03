4bdf7419a37f2675834a9c0044738dd69e9a0306
Alireza <26859725+tralireza@users.noreply.github.com>
2024-12-03 21:06:07 +0000

    std::time::{Duration, Instant}, match {};

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -39,4 +39,38 @@ mod tests {
         assert_eq!(Solution1346::check_if_exist(vec![10, 2, 5, 3]), true);
         assert_eq!(Solution1346::check_if_exist(vec![3, 1, 7, 11]), false);
     }
+
+    #[test]
+    fn test_constructs() {
+        for (_x, _y) in (0..8).zip(0..8) {}
+
+        let h = [1, 1, 3, 9, 42, 89, 113, 127];
+        for n in &h {
+            let rst = match n {
+                0 => "",
+                1..7 => "",
+                9 | 10 | 11 => "",
+                42 | 113 => "Y",
+                _ => "",
+            };
+            if rst == "Y" {
+                println!("{} => {}", rst, n)
+            }
+        }
+    }
+
+    #[test]
+    fn test_speed() {
+        use std::time::{Duration, Instant};
+
+        let mut count = 0;
+        let limit = Duration::new(1, 0);
+        let start = Instant::now();
+
+        while (Instant::now() - start) < limit {
+            count += 1
+        }
+
+        println!(" -> {count}")
+    }
 }
