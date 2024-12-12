1316555aa9ccbe060fe65518fbdeca6c18a938bf
Alireza <26859725+tralireza@users.noreply.github.com>
2024-12-12 00:02:19 +0000

    Binary Search 2779m Maximum Beauty of an Array After Applying Operation

--- binsearch/src/lib.rs
+++ binsearch/src/lib.rs
@@ -30,6 +30,37 @@ impl Solution1760 {
     }
 }
 
+// 2779m Maximum Beauty of an Array After Applying Operations
+struct Solution2779 {}
+
+impl Solution2779 {
+    pub fn maximum_beauty(nums: Vec<i32>, k: i32) -> i32 {
+        use std::iter::successors;
+
+        let mut nums = nums;
+        nums.sort();
+
+        successors(Some((0usize, 0usize)), |&(l, r)| {
+            let r = r + 1;
+            if r >= nums.len() {
+                None
+            } else if nums[r] - nums[l] <= 2*k {
+                Some((l, r))
+            } else {
+                let uval = nums[r];
+                let l = (l + 1..=r)
+                    .skip_while(|&i| uval - nums[i] > 2*k)
+                    .nth(0)
+                    .unwrap();
+                Some((l, r))
+            }
+        })
+            .map(|(l, r)| (r - l) as i32 + 1)
+            .max()
+            .unwrap_or(1)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,5 +69,11 @@ mod tests {
     fn test_solution1760() {
         assert_eq!(Solution1760::minimum_size(vec![9], 2), 3);
         assert_eq!(Solution1760::minimum_size(vec![2, 4, 8, 2], 4), 2);
+    }
+
+    #[test]
+    fn test_solution2779() {
+        assert_eq!(Solution2779::maximum_beauty(vec![4, 6, 1, 2], 2), 3);
+        assert_eq!(Solution2779::maximum_beauty(vec![1, 1, 1, 1], 10), 4);
     }
 }
