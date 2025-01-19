48a193cdcebb561d90307e910fc52b11a05c9233
Alireza <26859725+tralireza@users.noreply.github.com>
2025-01-19 16:56:39 +0000

    Math 908 Smallest Range I

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,5 +1,22 @@
 //! # Math (Core) :: Rusty
 
+/// 908 Smallest Range I
+struct Sol908;
+
+impl Sol908 {
+    pub fn smallest_range_i(nums: Vec<i32>, k: i32) -> i32 {
+        let mx = nums.iter().max();
+        let mn = nums.iter().min();
+
+        println!(" -> {:?} {:?}", mx, mn);
+
+        match (nums.iter().max(), nums.iter().min()) {
+            (Some(x), Some(n)) => 0.max(x - n - 2 * k),
+            _ => 0,
+        }
+    }
+}
+
 /// 989 Add to Array-Form of Integer
 struct Sol989;
 
@@ -49,6 +66,13 @@ impl Sol989 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_908() {
+        assert_eq!(Sol908::smallest_range_i(vec![1], 0), 0);
+        assert_eq!(Sol908::smallest_range_i(vec![0, 10], 2), 6);
+        assert_eq!(Sol908::smallest_range_i(vec![1, 3, 6], 3), 0);
+    }
 
     #[test]
     fn test_989() {
