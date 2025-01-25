e1c08c5ac4a882c081d1190c937648f8848894d3
Alireza <26859725+tralireza@users.noreply.github.com>
2025-01-25 21:56:38 +0000

    Sorting 2948m Make Lexicographically Smallest Array by Swapping Elements

--- sorting/src/lib.rs
+++ sorting/src/lib.rs
@@ -17,6 +17,42 @@ impl Sol905 {
     }
 }
 
+/// 2948m Make Lexicographically Smallest Array by Swapping Elements
+struct Sol2948;
+
+impl Sol2948 {
+    pub fn lexicographically_smallest_array(nums: Vec<i32>, limit: i32) -> Vec<i32> {
+        let mut nums: Vec<_> = nums.into_iter().enumerate().collect();
+        nums.sort_by_key(|t| t.1);
+        nums.push((nums.len() + 1, i32::MAX));
+
+        println!(" -> {:?}", nums);
+
+        let mut rst = vec![0; nums.len()];
+        let mut groups = vec![nums[0].0];
+        let mut p = 0;
+
+        (1..nums.len()).for_each(|i| {
+            if nums[i].1 > nums[i - 1].1 + limit {
+                groups.sort();
+                groups.reverse();
+
+                while let Some(g) = groups.pop() {
+                    rst[g] = nums[p].1;
+                    p += 1;
+                }
+
+                println!(" -> {:?}", rst);
+            }
+
+            groups.push(nums[i].0);
+        });
+
+        rst.pop();
+        rst
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,5 +64,21 @@ mod tests {
             vec![4, 2, 3, 1]
         );
         assert_eq!(Sol905::sort_array_by_parity(vec![0]), vec![0]);
+    }
+
+    #[test]
+    fn test_2948() {
+        assert_eq!(
+            Sol2948::lexicographically_smallest_array(vec![1, 5, 3, 9, 8], 2),
+            vec![1, 3, 5, 8, 9]
+        );
+        assert_eq!(
+            Sol2948::lexicographically_smallest_array(vec![1, 7, 6, 18, 2, 1], 3),
+            vec![1, 6, 7, 18, 1, 2]
+        );
+        assert_eq!(
+            Sol2948::lexicographically_smallest_array(vec![1, 7, 28, 19, 10], 2),
+            vec![1, 7, 28, 19, 10]
+        );
     }
 }
