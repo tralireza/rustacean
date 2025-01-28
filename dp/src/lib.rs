f0503ad62794717ed498aaaa1659cd4053cc39b7
Alireza <26859725+tralireza@users.noreply.github.com>
2025-01-28 17:43:33 +0000

    DP 2836h Maximize Value of Function in a Ball Passing Game -> Binary Lifting :: BITS

---
+++ dp/src/lib.rs
@@ -0,0 +1,61 @@
+//! # Dynamic Programming
+
+/// 2836h Maximize Value of Function in a Ball Passing Game
+struct Sol2836;
+
+impl Sol2836 {
+    pub fn get_max_function_value(receiver: Vec<i32>, k: i64) -> i64 {
+        // Binary Lifting
+        // far(p, i) :: 2^p ancestor of i
+        // far(p, i) = far(p-1, far(p-1, i))
+
+        println!(" || {:?}", receiver);
+
+        let (bits, nodes) = (k.ilog2() as usize + 1, receiver.len());
+
+        let mut far = vec![vec![0; bits]; nodes];
+        (0..bits).for_each(|p| {
+            (0..nodes).for_each(|i| match p {
+                0 => far[i][0] = receiver[i] as usize,
+                _ => far[i][p] = far[far[i][p - 1]][p - 1],
+            })
+        });
+
+        println!(" -> {:?}", far);
+
+        let mut score = vec![vec![0i64; bits]; nodes];
+        (0..bits).for_each(|p| {
+            (0..nodes).for_each(|i| match p {
+                0 => score[i][0] = receiver[i] as i64,
+                _ => score[i][p] = score[i][p - 1] + score[far[i][p - 1]][p - 1],
+            })
+        });
+
+        println!(" -> {:?}", score);
+
+        (0..nodes).fold(0, |xscore, istart| {
+            xscore.max({
+                let (mut iscore, mut i) = (0, istart);
+                (0..bits).rev().for_each(|p| {
+                    if ((1 << p) & k) != 0 {
+                        iscore += score[i][p];
+                        i = far[i][p];
+                    }
+                });
+                iscore + istart as i64
+            })
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_2836() {
+        // 1 <= Receiver.Length <= 10^5,  1 <= k <= 10^10
+        assert_eq!(Sol2836::get_max_function_value(vec![2, 0, 1], 4), 6);
+        assert_eq!(Sol2836::get_max_function_value(vec![1, 1, 1, 2, 3], 3), 10);
+    }
+}
