99ef05794c78eb346718bf65a50a49a360f74974
Alireza <26859725+tralireza@users.noreply.github.com>
2025-02-12 05:39:55 +0000

    Binary Search 153m Find Minimum in Rotated Sorted Array

--- bsearch/src/lib.rs
+++ bsearch/src/lib.rs
@@ -1,5 +1,26 @@
 //! # Rust :: Binary Search
 
+/// 153m Find Minimum in Rotated Sorted Array
+struct Sol153;
+
+impl Sol153 {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let (mut l, mut r) = (0, nums.len() - 1);
+
+        while l < r {
+            let m = l + ((r - l) >> 1);
+
+            if nums[m] > nums[r] {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        nums[l]
+    }
+}
+
 /// 154h Find Minimum in Rotated Sorted Array II
 struct Sol154;
 
@@ -26,6 +47,13 @@ impl Sol154 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_153() {
+        assert_eq!(Sol153::find_min(vec![3, 4, 5, 1, 2]), 1);
+        assert_eq!(Sol153::find_min(vec![4, 5, 6, 7, 0, 1, 2]), 0);
+        assert_eq!(Sol153::find_min(vec![11, 13, 15, 17]), 11);
+    }
 
     #[test]
     fn test_154() {
