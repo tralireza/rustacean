9961ceea1c7952e3315c5f80fdf40444f93029a6
Alireza <26859725+tralireza@users.noreply.github.com>
2025-03-03 01:43:23 +0000

    Simulation 2161m Partition Array According to Given Pivot

--- simulation/src/lib.rs
+++ simulation/src/lib.rs
@@ -1,5 +1,30 @@
 //! # Rust :: Simulation
 
+/// 2161m Partition Array According to Given Pivot
+struct Sol2161;
+
+impl Sol2161 {
+    pub fn pivot_array(nums: Vec<i32>, pivot: i32) -> Vec<i32> {
+        let mut rst = vec![pivot; nums.len()];
+
+        let (mut left, mut right) = (0, nums.len() - 1);
+        for (l, r) in (0..nums.len()).zip((0..nums.len()).rev()) {
+            if nums[l] < pivot {
+                rst[left] = nums[l];
+                left += 1;
+            }
+            if nums[r] > pivot {
+                rst[right] = nums[r];
+                right -= 1;
+            }
+        }
+
+        println!(":: {:?}", rst);
+
+        rst
+    }
+}
+
 /// 2460 Apply Operations to an Array
 struct Sol2460;
 
@@ -41,6 +66,18 @@ impl Sol2460 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_2161() {
+        assert_eq!(
+            Sol2161::pivot_array(vec![9, 12, 5, 10, 14, 3, 10], 10),
+            vec![9, 5, 3, 10, 10, 12, 14]
+        );
+        assert_eq!(
+            Sol2161::pivot_array(vec![-3, 4, 3, 2], 2),
+            vec![-3, 2, 4, 3]
+        );
+    }
 
     #[test]
     fn test_2460() {
