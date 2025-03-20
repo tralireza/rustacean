2624c17cee8efec6fca35f53784c30015ec387f7
Alireza <26859725+tralireza@users.noreply.github.com>
2025-03-20 19:46:41 +0000

    Graph, Disjoint Set 3108h Minimum Cost Walk in Weighted Graph

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -845,264 +845,71 @@ impl Sol2658 {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// 3108h Minimum Cost Walk in Weighted Graph
+struct Sol3108;
 
-    #[test]
-    fn test_684() {
-        for f in [
-            Sol684::find_redundant_connection,
-            Sol684::find_redundant_connection_graph,
-        ] {
-            assert_eq!(f(vec![vec![1, 2], vec![1, 3], vec![2, 3]]), vec![2, 3]);
-            assert_eq!(
-                f(vec![
-                    vec![1, 2],
-                    vec![2, 3],
-                    vec![3, 4],
-                    vec![1, 4],
-                    vec![1, 5]
-                ]),
-                vec![1, 4]
-            );
+impl Sol3108 {
+    pub fn minimum_cost(n: i32, edges: Vec<Vec<i32>>, query: Vec<Vec<i32>>) -> Vec<i32> {
+        use std::cmp::Ordering::*;
 
-            assert_eq!(
-                f(vec![
-                    vec![7, 8],
-                    vec![2, 6],
-                    vec![2, 8],
-                    vec![1, 4],
-                    vec![9, 10],
-                    vec![1, 7],
-                    vec![3, 9],
-                    vec![6, 9],
-                    vec![3, 5],
-                    vec![3, 10]
-                ]),
-                vec![3, 10]
-            );
+        let mut djset: Vec<usize> = (0..n as usize).collect();
+        let mut ranks = vec![0; n as usize];
+        let mut weights = vec![usize::MAX; n as usize];
+
+        fn find(djset: &mut Vec<usize>, x: usize) -> usize {
+            if djset[x] != x {
+                djset[x] = find(djset, djset[x]);
+            }
+            djset[x]
         }
-    }
 
-    #[test]
-    fn test_695() {
-        assert_eq!(
-            Sol695::max_area_of_island(vec![
-                vec![0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-                vec![0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                vec![0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-                vec![0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-                vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-                vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-                vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
-            ]),
-            6
-        );
-        assert_eq!(
-            Sol695::max_area_of_island(vec![vec![0, 0, 0, 0, 0, 0, 0, 0]]),
-            0
-        );
-    }
-
-    #[test]
-    fn test_802() {
-        assert_eq!(
-            Sol802::eventual_safe_nodes(vec![
-                vec![1, 2],
-                vec![2, 3],
-                vec![5],
-                vec![0],
-                vec![5],
-                vec![],
-                vec![]
-            ]),
-            vec![2, 4, 5, 6]
-        );
-        assert_eq!(
-            Sol802::eventual_safe_nodes(vec![
-                vec![1, 2, 3, 4],
-                vec![1, 2],
-                vec![3, 4],
-                vec![0, 4],
-                vec![]
-            ]),
-            vec![4]
-        );
-    }
-
-    #[test]
-    fn test_827() {
-        assert_eq!(Sol827::largest_island(vec![vec![1, 0], vec![0, 1]]), 3);
-        assert_eq!(Sol827::largest_island(vec![vec![1, 1], vec![1, 0]]), 4);
-        assert_eq!(Sol827::largest_island(vec![vec![1, 1], vec![1, 1]]), 4);
-    }
-
-    #[test]
-    fn test_1267() {
-        assert_eq!(Sol1267::count_servers(vec![vec![1, 0], vec![0, 1]]), 0);
-        assert_eq!(Sol1267::count_servers(vec![vec![1, 0], vec![1, 1]]), 3);
-        assert_eq!(
-            Sol1267::count_servers(vec![
-                vec![1, 1, 0, 0],
-                vec![0, 0, 1, 0],
-                vec![0, 0, 1, 0],
-                vec![0, 0, 0, 1]
-            ]),
-            4
-        );
-    }
-
-    #[test]
-    fn test_1368h() {
-        for f in [Sol1368::min_cost, Sol1368::min_cost_bfs01] {
-            assert_eq!(
-                f(vec![
-                    vec![1, 1, 1, 1],
-                    vec![2, 2, 2, 2],
-                    vec![1, 1, 1, 1],
-                    vec![2, 2, 2, 2]
-                ]),
-                3
-            );
-            assert_eq!(f(vec![vec![1, 1, 3], vec![3, 2, 2], vec![1, 1, 4]]), 0);
-            assert_eq!(f(vec![vec![1, 2], vec![4, 3]]), 1);
+        fn union(djset: &mut Vec<usize>, ranks: &mut Vec<usize>, x: usize, y: usize) -> usize {
+            let x = find(djset, x);
+            let y = find(djset, y);
+            match x.cmp(&y) {
+                Equal => x,
+                _ => match ranks[x].cmp(&ranks[y]) {
+                    Less => {
+                        djset[x] = y;
+                        y
+                    }
+                    _ => {
+                        djset[y] = x;
+                        if ranks[x] == ranks[y] {
+                            ranks[x] += 1;
+                        }
+                        x
+                    }
+                },
+            }
         }
-    }
 
-    #[test]
-    fn test_1462() {
-        assert_eq!(
-            Sol1462::check_if_prerequisite(2, vec![vec![1, 0]], vec![vec![0, 1], vec![1, 0]]),
-            vec![false, true]
-        );
-        assert_eq!(
-            Sol1462::check_if_prerequisite(2, vec![], vec![vec![1, 0], vec![0, 1]]),
-            vec![false, false]
-        );
-        assert_eq!(
-            Sol1462::check_if_prerequisite(
-                3,
-                vec![vec![1, 2], vec![0, 1], vec![2, 0]],
-                vec![vec![1, 0], vec![1, 2]]
-            ),
-            vec![true, true]
-        );
-    }
-
-    #[test]
-    fn test_1765() {
-        assert_eq!(
-            Sol1765::highest_peak(vec![vec![0, 1], vec![0, 0]]),
-            vec![vec![1, 0], vec![2, 1]]
-        );
-        assert_eq!(
-            Sol1765::highest_peak(vec![vec![0, 0, 1], vec![1, 0, 0], vec![0, 0, 0]]),
-            vec![vec![1, 1, 0], vec![0, 1, 1], vec![1, 2, 2]]
-        );
-    }
-
-    #[test]
-    fn test_2127() {
-        assert_eq!(Sol2127::maximum_invitations(vec![2, 2, 1, 2]), 3);
-        assert_eq!(Sol2127::maximum_invitations(vec![1, 2, 0]), 3);
-        assert_eq!(Sol2127::maximum_invitations(vec![3, 0, 1, 4, 1]), 4);
-    }
-
-    #[test]
-    fn test_2467() {
-        assert_eq!(
-            Sol2467::most_profitable_path(
-                vec![vec![0, 1], vec![1, 2], vec![1, 3], vec![3, 4]],
-                3,
-                vec![-2, 4, 2, -4, 6]
-            ),
-            6
-        );
-        assert_eq!(
-            Sol2467::most_profitable_path(vec![vec![0, 1]], 1, vec![-7280, 2350]),
-            -7280
-        );
-        assert_eq!(
-            Sol2467::most_profitable_path(
-                vec![vec![0, 1], vec![0, 2]],
-                2,
-                vec![-3360, -5394, -1146]
-            ),
-            -3360
-        );
-    }
-
-    #[test]
-    fn test_2493() {
-        assert_eq!(
-            Sol2493::magnificent_sets(
-                6,
-                vec![
-                    vec![1, 2],
-                    vec![1, 4],
-                    vec![1, 5],
-                    vec![2, 6],
-                    vec![2, 3],
-                    vec![4, 6]
-                ]
-            ),
-            4
-        );
-        assert_eq!(
-            Sol2493::magnificent_sets(3, vec![vec![1, 2], vec![2, 3], vec![3, 1],]),
-            -1
-        );
-    }
-
-    #[test]
-    fn test_2608() {
-        assert_eq!(
-            Sol2608::find_shortest_cycle(
-                7,
-                vec![
-                    vec![0, 1],
-                    vec![1, 2],
-                    vec![2, 0],
-                    vec![3, 4],
-                    vec![4, 5],
-                    vec![5, 6],
-                    vec![6, 3]
-                ]
-            ),
-            3
-        );
-        assert_eq!(
-            Sol2608::find_shortest_cycle(4, vec![vec![0, 1], vec![0, 2]]),
-            -1
-        );
-    }
-
-    #[test]
-    fn test_2658() {
-        for f in [Sol2658::find_max_fish, Sol2658::find_max_fish_recursion] {
-            assert_eq!(
-                f(vec![
-                    vec![0, 2, 1, 0],
-                    vec![4, 0, 0, 3],
-                    vec![1, 0, 0, 4],
-                    vec![0, 3, 2, 0]
-                ]),
-                7
-            );
-            assert_eq!(
-                f(vec![
-                    vec![1, 0, 0, 0],
-                    vec![0, 0, 0, 0],
-                    vec![0, 0, 0, 0],
-                    vec![0, 0, 0, 1]
-                ]),
-                1
-            );
-
-            assert_eq!(f(vec![vec![4, 5, 5], vec![0, 10, 0],]), 24);
-            assert_eq!(f(vec![vec![8, 6], vec![2, 6]]), 22);
+        for v in &edges {
+            union(&mut djset, &mut ranks, v[0] as usize, v[1] as usize);
         }
+
+        for v in &edges {
+            let r = find(&mut djset, v[0] as usize);
+            weights[r] &= v[2] as usize;
+        }
+
+        println!("-> {:?}", (&djset, &ranks, &weights));
+
+        let mut rst = vec![];
+        for v in query {
+            let (x, y) = (
+                find(&mut djset, v[0] as usize),
+                find(&mut djset, v[1] as usize),
+            );
+            match x.cmp(&y) {
+                Equal => rst.push(weights[x] as i32),
+                _ => rst.push(-1),
+            }
+        }
+
+        rst
     }
 }
+
+#[cfg(test)]
+mod tests;
