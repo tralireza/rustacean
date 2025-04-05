dbe7abd3c9634556683ad958d8a5be3adf7f518a
Alireza <26859725+tralireza@users.noreply.github.com>
2025-04-05 11:37:33 +0100

    BIT 218h The Skyline Problems -> Ordered List (BTreeSet,BTreeMap)

--- bit/src/lib.rs
+++ bit/src/lib.rs
@@ -53,31 +53,5 @@ impl Sol218 {
     }
 }
 
-/// 1863 Sum of All Subset XOR Totals
-struct Sol1863;
-
-impl Sol1863 {
-    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
-        fn search(nums: &[i32], start: usize, xor: i32) -> i32 {
-            if start == nums.len() {
-                return xor;
-            }
-
-            search(nums, start + 1, xor) + search(nums, start + 1, nums[start] ^ xor)
-        }
-
-        search(&nums, 0, 0)
-    }
-
-    fn subset_xor_sum_bitwise(nums: Vec<i32>) -> i32 {
-        let mut xsum = 0;
-        for n in &nums {
-            xsum |= n;
-        }
-
-        xsum << (nums.len() - 1)
-    }
-}
-
 #[cfg(test)]
 mod tests;
--- bit/src/tests.rs
+++ bit/src/tests.rs
@@ -2,44 +2,34 @@ use super::*;
 
 #[test]
 fn test_218() {
-    assert_eq!(
-        Sol218::get_skyline(vec![
-            vec![2, 9, 10],
-            vec![3, 7, 15],
-            vec![5, 12, 12],
-            vec![15, 20, 10],
-            vec![19, 24, 8]
-        ]),
-        vec![
-            vec![2, 10],
-            vec![3, 15],
-            vec![7, 12],
-            vec![12, 0],
-            vec![15, 10],
-            vec![20, 8],
-            vec![24, 0]
-        ]
-    );
-    assert_eq!(
-        Sol218::get_skyline(vec![vec![0, 2, 3], vec![2, 5, 3]]),
-        vec![vec![0, 3], vec![5, 0]]
-    );
-
-    assert_eq!(
-        Sol218::get_skyline(vec![vec![2, 9, 10], vec![12, 15, 10]]),
-        vec![vec![2, 10], vec![9, 0], vec![12, 10], vec![15, 0]]
-    );
-}
-
-#[test]
-fn test_1863() {
-    for (rst, nums) in [
-        (6, vec![1, 3]),
-        (28, vec![5, 1, 6]),
-        (480, vec![3, 4, 5, 6, 7, 8]),
+    for (rst, buildings) in [
+        (
+            vec![
+                vec![2, 10],
+                vec![3, 15],
+                vec![7, 12],
+                vec![12, 0],
+                vec![15, 10],
+                vec![20, 8],
+                vec![24, 0],
+            ],
+            vec![
+                vec![2, 9, 10],
+                vec![3, 7, 15],
+                vec![5, 12, 12],
+                vec![15, 20, 10],
+                vec![19, 24, 8],
+            ],
+        ),
+        (
+            vec![vec![0, 3], vec![5, 0]],
+            vec![vec![0, 2, 3], vec![2, 5, 3]],
+        ),
+        (
+            vec![vec![2, 10], vec![9, 0], vec![12, 10], vec![15, 0]],
+            vec![vec![2, 9, 10], vec![12, 15, 10]],
+        ),
     ] {
-        for f in [Sol1863::subset_xor_sum, Sol1863::subset_xor_sum_bitwise] {
-            assert_eq!(f(nums.to_vec()), rst);
-        }
+        assert_eq!(Sol218::get_skyline(buildings), rst);
     }
 }
