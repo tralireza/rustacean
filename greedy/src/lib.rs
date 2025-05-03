f5f88e783eb54428213df8b40a2e9ed7bb6e7d9a
Alireza <26859725+tralireza@users.noreply.github.com>
2025-05-03 10:17:05 +0100

    Greedy 1007m Minimum Domino Rotations For Equal Row

--- greedy/src/lib.rs
+++ greedy/src/lib.rs
@@ -18,5 +18,39 @@ impl Sol781 {
     }
 }
 
+/// 1007m Minimum Domino Rotations For Equal Row
+struct Sol1007;
+
+impl Sol1007 {
+    pub fn min_domino_rotations(tops: Vec<i32>, bottoms: Vec<i32>) -> i32 {
+        let mut r = i32::MAX;
+
+        'LOOP: for n in 1..=6 {
+            let mut top = 0;
+            let mut bottom = 0;
+
+            for (&t, &b) in tops.iter().zip(bottoms.iter()) {
+                if t != n && b != n {
+                    continue 'LOOP;
+                }
+
+                if t != n {
+                    top += 1;
+                }
+                if b != n {
+                    bottom += 1;
+                }
+            }
+
+            r = r.min(top.min(bottom));
+        }
+
+        if r < i32::MAX {
+            return r;
+        }
+        -1
+    }
+}
+
 #[cfg(test)]
 mod tests;
--- greedy/src/tests.rs
+++ greedy/src/tests.rs
@@ -6,3 +6,13 @@ fn test_781() {
         assert_eq!(Sol781::num_rabbits(answers), rst);
     }
 }
+
+#[test]
+fn test_1007() {
+    for (rst, tops, bottoms) in [
+        (2, vec![2, 1, 2, 4, 2, 2], vec![5, 2, 6, 2, 3, 2]),
+        (-1, vec![3, 5, 1, 2, 3], vec![3, 6, 3, 3, 4]),
+    ] {
+        assert_eq!(Sol1007::min_domino_rotations(tops, bottoms), rst);
+    }
+}
